3c5395afd8f758e5fcdb11fd0d8d830889199a09
jack1234-cloud <jack.oflynn@hotmail.com>
2022-06-15 12:20:22 +0100

    updates

---
+++ ec2/ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "******"
+  secret_key = "******"
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "myec2" {
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
